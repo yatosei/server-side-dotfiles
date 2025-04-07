a882d98848013a7b852c9c57602613d8c725cdba
yatosei <52796268+yatosei@users.noreply.github.com.>
2025-04-07 22:39:07 +0900

    wip

---
+++ install-vbox-guest-additions.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]; then
+  echo "このスクリプトはrootまたはsudoで実行してください。"
+  exit 1
+fi
+
+if [ ! -e /dev/cdrom ]; then
+  echo "Guest Additions のISOを挿入してください。"
+  exit 1
+fi
+
+# マウント
+echo "Guest Additions ISOをマウントします..."
+mkdir -p /mnt/cdrom
+mount /dev/cdrom /mnt/cdrom
+
+if [ ! -f /mnt/cdrom/VBoxLinuxAdditions.run ]; then
+  echo "Guest Additions以外のISOがマウントされています。"
+  exit 1
+fi
+
+# インストール
+echo "Guest Additions をインストール中..."
+sh /mnt/cdrom/VBoxLinuxAdditions.run
+
+# アンマウント
+echo "マウント解除..."
+umount /mnt/cdrom
+
+# 共有フォルダのグループ追加
+usermod -aG vboxsf ${USER}
+
+echo "インストール完了。再起動してください。"
